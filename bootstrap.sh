0ea8863504d7da264eae2b9d40a9151569617abf
gonzolively <knox.lively@gmail.com>
2020-04-08 00:02:25 -0500

    Removed unnecessary steps

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,8 +16,6 @@ fi
 if [ "$DISTRO" == "Ubuntu" ]; 
 then
    echo "This is Ubuntu"
-   #sudo apt-get update &&
-   #sudo apt-get -y install software-properties-common &&
    #sudo apt-add-repository -y ppa:ansible/ansible &&
    #sudo apt-get update &&
    #sudo apt-get install -y ansible &&
