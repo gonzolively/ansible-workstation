2dee338846cf3bdc73f244b732f55ba99004ca91
Knox Lively <knox.lively@gmail.com>
2020-04-26 17:39:57 -0500

    added debuggging for stupid GUI

--- roles/dotfiles/files/deploy-dotfiles.sh
+++ roles/dotfiles/files/deploy-dotfiles.sh
@@ -11,7 +11,9 @@ function config {
 
 echo "Backing up pre-existing dot files..."
 mkdir -p $backup_dir &&
+# TODO: Add a bit to create the existing directory structure (empty) before copying config over
 config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I {} mv $HOME/{} $backup_dir/{} &&
+#config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I {} mkdir -p $backup_dir/{}; mv $HOME/{} $backup_dir/{} &&
 
 config checkout &&
 config config status.showUntrackedFiles no
