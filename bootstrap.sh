10bf5f7e033d7d6aadc374d6cfcd393681abfa31
Knox Lively <knox@animal.local>
2024-02-05 15:34:16 -0700

    refactored bootstrap script to be more robust

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 
+#!/usr/bin/env bash
 
-# This script is intended to be ran after a fresh os install in order to install ansible and start the deployment process.
+# This script is intended to be run after a fresh OS install to install Ansible and start the deployment process.
 command="ansible-playbook -l localhost deploy.yml --vault-password-file .vault_pass.txt --ask-become-pass"
 
-# If available, use LSB to identify distribution
-if [ -f /etc/lsb-release -o -d /etc/lsb-release.d ]; then
-   DISTRO=$(lsb_release -i | cut -d: -f2 | sed s/'^\t'//)
-# Otherwise, use release info file
+# Determine distribution
+if command -v lsb_release >/dev/null; then
+    DISTRO=$(lsb_release -is)
 else
-   DISTRO=$(ls -d /etc/[A-Za-z]*[_-][rv]e[lr]* | grep -v "lsb" | cut -d'/' -f3 | cut -d'-' -f1 | cut -d'_' -f1)
+    DISTRO=$(uname -s)
 fi
 
-# Run platform specific commands
-if [ "$DISTRO" == "Ubuntu" ]; 
-then
-   echo -e "\nInstalling Ansible...\n" &&
-   sudo apt-add-repository -r ppa:ansible/ansible -y &&
-   sudo apt-get update &&
-   sudo apt-get install -y ansible &&
-   echo -e "\nAnsible installed successfully, now running playbook...\n" &&
-   $command
-elif [ "$DISTRO" == "Fedora" ]; 
-then
-   echo "This is Fedora"
-elif [ "$DISTRO" == "ArchLinux" ];
-then
-   echo "This is Archlinux"
-else
-   echo "I am not sure what this is"
-fi
+echo "Detected distribution: $DISTRO"
+
+# Run platform-specific commands
+case $DISTRO in
+    Ubuntu)
+        echo -e "\nInstalling Ansible...\n"
+        sudo apt update
+        sudo apt install -y software-properties-common
+        sudo apt-add-repository --yes --update ppa:ansible/ansible
+        sudo apt install -y ansible
+        echo -e "\nAnsible installed successfully, now running playbook...\n"
+        eval "$command"
+        ;;
+    Fedora)
+        echo "This is Fedora"
+        ;;
+    ArchLinux|Arch)
+        echo "This is Arch Linux"
+        ;;
+    *)
+        echo "Unsupported distribution: $DISTRO"
+        ;;
+esac
